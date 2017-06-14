68213f0fc10f44217bd00c7e5f814c3d3845b118
saqibarfeen <saqib.arfeen@cloud9net.com>
2017-06-14 09:44:27 +0500

    minor changes

--- iota-elasticsearch-kibana/scripts/script-de-logstash.sh
+++
@@ -1,5 +0,0 @@
-mkdir ~/workspace && cd ~/workspace
-git clone https://saqibarfeen:cloud9net@github.com/cloud9network/observer9.git
-cd observer9
-mkdir -p /etc/logstash/conf.d
-cp ./logging/logstash_config/* /etc/logstash/conf.d
